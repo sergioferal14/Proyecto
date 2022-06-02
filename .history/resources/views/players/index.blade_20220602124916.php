d3d562fdf802d68ebfe5de7c3cefa52d485b776e
= <=>
2022-06-02 14:41:10 +0200

    Falta storage

---
+++ .history/resources/views/players/index.blade_20220602124916.php
@@ -0,0 +1,133 @@
+<x-app-layout>
+    
+    <div class="mt-4 max-w-7xl mx-auto sm:px-6 lg:px-8 container" >
+        
+        <div class="my-2 mb-5 grid md:grid-cols-2" style="min-height:calc(100vh-266px)">
+            <div class=" card2 xl:w-96 ml-6 bg-black text-white mr-2 mx-w-sm rounded overflow-hidden shadow-lg ">
+  
+                <div class="text-lg flex flex-row justify-top text-center"
+                    style="background-color: #0288d1;color:white;">
+                    <h3 class="font-bold px-6 py-4 ">Jugadores del {{$team->nombre}}</h3><img src="{{ asset('storage/'.$team->escudo) }}" class="mr-3 h-12 sm:h-12 mx-auto my-auto flex-end">
+                </div>
+                
+                <div>
+                    <x-tabla>
+                        <table role="grid" class="min-w-full divide-y divide-gray-200">
+                            <thead>
+                                <tr role="row">
+                                    <form action="{{route('players.index',$team)}}" method="GET">
+                                    
+                                    <th
+                                        style="text-align: center;background-color: white; border:none; outline:none;padding:10px;font-size:16px;">
+                                        <input  name="nombre" id="search" value="{{$request->nombre}}"
+                                            style="padding: 9px 10px;width: 100%; display: block;margin: 0;outline: medium none; border-bottom: solid 1px #c6d6df;"
+                                            class="focus:bg-white focus:border-blue-600 focus:outline-none"
+                                            placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
+  
+                                    </th>
+                                    <th>
+                                        <button type="submit" class="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                        
+                                    </th>
+                                </form>
+                                </tr>
+                            
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200 overflow-auto" >
+  
+                                @if ($players->count())
+                                    @foreach ($players as $item)
+                                    <div>
+                                        @if($control==false)
+                                        @if($item->id==$player->id)
+                                        <tr class=" border-b bg-sky-400 dark:hover:bg-sky-300 " style="overflow: scroll">
+                                        @else
+                                        <tr class=" border-b hover:bg-sky-50 dark:hover:bg-sky-200" style="overflow: scroll">
+                                        @endif
+                                        @else
+                                        <tr class=" border-b hover:bg-sky-50 dark:hover:bg-sky-300" style="overflow: scroll">
+                                        @endif
+                                            
+                                                <td role="gridcell">
+                                                    <a href="{{ route('players.edit',[$team,$item]) }}">
+                                                    <div style="display: flex;align-items:center;">
+                                                        <div class="flex">
+                                                            <img class="h-12 w-12 ml-1 object-cover"
+                                                                src="{{ asset('storage/' . $item->foto) }}" style="background-color: white !important;
+                                                                border-right: none !important;
+                                                                border-top: none !important;
+                                                                border-bottom: none !important;
+                                                                height: calc(60px+1vw);
+                                                                border-radius: 4px;
+                                                                margin-right: 10px;
+                                                                border-width: 4px !important;" alt="">
+                                                        </div>
+                                                        <div class="ml-4">
+                                                            <div class="text-sm font-medium text-gray-900 ">
+                                                                {{ $item->nombre }} {{ $item->apellidos }}
+                                                            </div>
+                                                            <div class="text-sm text-gray-500">{{ $item->posicion }}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    {{ $item->dorsal }}
+                                                </td>
+                                            </a>
+                                        </tr>
+                                    
+                                    </div>
+                                    @endforeach
+                                @else
+                                <tr class="border-b">
+                                    <td class="mt-2 text-center font-bold text-lg">No se enconcotró ningún jugador.
+                                    </td>
+                                </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </x-tabla>
+  
+                </div>
+                <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-right">
+                    <div class=" mx-auto pagination ">
+                        {{ $players->links() }}
+                    </div>
+                </div>
+            </div>
+            <div>
+                @if ($control==true)
+                    @include('players.create')
+                @else
+                
+                    @include('players.edit')
+                @endif
+            </div>
+        </div>
+    </div>
+    @if (session('crear'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: '{{ session('crear') }}',
+                showConfirmButton: false,
+                timer: 2500
+            })
+        </script>
+        
+        @endif
+        @if (session('borrar'))
+        <script>
+            Swal.fire({
+                icon: 'error',
+                title: '{{ session('borrar') }}',
+                showConfirmButton: false,
+                timer: 2500
+            })
+        </script>
+        @endif
+  </x-app-layout>
+  
---
+++ .history/resources/views/profile/two-factor-authentication-form.blade_20220602113901.php
@@ -0,0 +1,132 @@
+<x-jet-action-section>
+    <x-slot name="title">
+        <span class="m-2" style="whi">{{ __('Verificacion en dos pasos') }}</span>
+        
+    </x-slot>
+
+    <x-slot name="description">
+        {{ __('Agregue seguridad adicional a su cuenta mediante la autenticación de dos factores.
+        ') }}
+    </x-slot>
+
+    <x-slot name="content">
+        <h3 class="text-lg font-bold">
+            @if ($this->enabled)
+                @if ($showingConfirmation)
+                    {{ __('Termine de habilitar la autenticación de dos pasos.') }}
+                @else
+                    {{ __('Has activado la verificación en dos pasos.') }}
+                @endif
+            @else
+                {{ __(' No has activado la verificación en dos pasos.') }}
+            @endif
+        </h3>
+
+        <div class="mt-3 max-w-xl text-base ">
+            <p>
+                {{ __('Cuando la autenticación de dos factores está habilitada, se le solicitará un token seguro y aleatorio durante la autenticación. Puede recuperar este token de la aplicación Google Authenticator de su teléfono.
+                ') }}
+            </p>
+        </div>
+
+        @if ($this->enabled)
+            @if ($showingQrCode)
+                <div class="mt-4 max-w-xl text-sm text-gray-600">
+                    <p class="font-semibold">
+                        @if ($showingConfirmation)
+                            {{ __('Para terminar de habilitar la autenticación de dos factores, escanee el siguiente código QR usando la aplicación de autenticación de su teléfono o ingrese la clave de configuración y proporcione el código OTP generado.
+                            ') }}
+                        @else
+                            {{ __('La autenticación de dos factores ahora está habilitada. Escanee el siguiente código QR usando la aplicación de autenticación de su teléfono o ingrese la clave de configuración.
+                            ') }}
+                        @endif
+                    </p>
+                </div>
+
+                <div class="mt-4">
+                    {!! $this->user->twoFactorQrCodeSvg() !!}
+                </div>
+
+                <div class="mt-4 max-w-xl text-sm text-gray-600">
+                    <p class="font-semibold">
+                        {{ __('Clave de configuración') }}: {{ decrypt($this->user->two_factor_secret) }}
+                    </p>
+                </div>
+
+                @if ($showingConfirmation)
+                    <div class="mt-4">
+                        <x-jet-label for="code" value="{{ __('Codigo') }}" />
+
+                        <x-jet-input id="code" type="text" name="code" class="block mt-1 w-1/2" inputmode="numeric" autofocus autocomplete="one-time-code"
+                            wire:model.defer="code"
+                            wire:keydown.enter="confirmTwoFactorAuthentication" />
+
+                        <x-jet-input-error for="code" class="mt-2" />
+                    </div>
+                @endif
+            @endif
+
+            @if ($showingRecoveryCodes)
+                <div class="mt-4 max-w-xl text-sm text-gray-600">
+                    <p class="font-semibold">
+                        {{ __('Guarde estos códigos de recuperación en un administrador de contraseñas seguro. Se pueden usar para recuperar el acceso a su cuenta si se pierde su dispositivo de autenticación de dos factores.
+                        ') }}
+                    </p>
+                </div>
+
+                <div class="grid gap-1 max-w-xl mt-4 px-4 py-4 font-mono text-sm bg-gray-100 rounded-lg">
+                    @foreach (json_decode(decrypt($this->user->two_factor_recovery_codes), true) as $code)
+                        <div>{{ $code }}</div>
+                    @endforeach
+                </div>
+            @endif
+        @endif
+
+        <div class="mt-5">
+            @if (! $this->enabled)
+                <x-jet-confirms-password wire:then="enableTwoFactorAuthentication">
+                    <x-jet-button type="button" wire:loading.attr="disabled">
+                        {{ __('Activar') }}
+                    </x-jet-button>
+                </x-jet-confirms-password>
+            @else
+                @if ($showingRecoveryCodes)
+                    <x-jet-confirms-password wire:then="regenerateRecoveryCodes">
+                        <x-jet-secondary-button class="mr-3">
+                            {{ __('Regenerar códigos de recuperación
+                            ') }}
+                        </x-jet-secondary-button>
+                    </x-jet-confirms-password>
+                @elseif ($showingConfirmation)
+                    <x-jet-confirms-password wire:then="confirmTwoFactorAuthentication">
+                        <x-jet-button type="button" class="mr-3" wire:loading.attr="disabled">
+                            {{ __('Confirmar') }}
+                        </x-jet-button>
+                    </x-jet-confirms-password>
+                @else
+                    <x-jet-confirms-password wire:then="showRecoveryCodes">
+                        <x-jet-secondary-button class="mr-3">
+                            {{ __('Mostrar códigos de recuperación
+                            ') }}
+                        </x-jet-secondary-button>
+                    </x-jet-confirms-password>
+                @endif
+
+                @if ($showingConfirmation)
+                    <x-jet-confirms-password wire:then="disableTwoFactorAuthentication">
+                        <x-jet-secondary-button wire:loading.attr="disabled">
+                            {{ __('Cancelar') }}
+                        </x-jet-secondary-button>
+                    </x-jet-confirms-password>
+                @else
+                    <x-jet-confirms-password wire:then="disableTwoFactorAuthentication">
+                        <x-jet-danger-button wire:loading.attr="disabled">
+                            {{ __('Desactivar') }}
+                        </x-jet-danger-button>
+                    </x-jet-confirms-password>
+                @endif
+
+            @endif
+        </div>
+    </x-slot>
+</x-jet-action-section>

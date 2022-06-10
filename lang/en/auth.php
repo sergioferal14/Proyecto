251ab9df6ef1b01f1f08559e0957eff15c9f9f7e
= <=>
2022-06-10 09:17:24 +0200

    Ultimo Commit

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Las credenciales no coinciden.',
+    'password' => 'La contraseñ es incorrecta.',
+    'throttle' => 'Demasiados intentos fallidos.Pruebe otra vez en :seconds segundos.',
 
 ];

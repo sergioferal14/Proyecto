e2968636affbfba7887d0d2866adee6ab1b3369d
= <=>
2022-06-13 10:30:04 +0200

    Ultimo Commit

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -29,7 +29,7 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{asset('storage/'.$this->user->profile_photo_path) }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
@@ -40,9 +40,8 @@
                 </div>
 
                 <x-jet-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
-                    {{ __('Cambiar Foto') }}
+                    {{ __('Añadir Foto') }}
                 </x-jet-secondary-button>
-
 
                 <x-jet-input-error for="photo" class="mt-2" />
             </div>
